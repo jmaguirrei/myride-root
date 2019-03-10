aa4e2c141f765f5d04fca6d417e211b6a40ae4e3
José Manuel Aguirre <jmaguirrei@gmail.com>
2019-03-10 17:28:56 -0300

    Initial commit

--- js/sw/cacheFiles.js
+++ js/sw/cacheFiles.js
@@ -4,20 +4,20 @@ export const cacheFilesBefore = [
   './',
 
   // icons needed early (in manifest or favicons)
-  '../icons/icon_32x32.png',
-  '../icons/icon_16x16.png',
-  '../icons/icon_192x192.png',
-  '../icons/icon_256x256.png',
-  '../icons/icon_512x512.png',
+  './assets/icons/icon_32x32.png',
+  './assets/icons/icon_16x16.png',
+  './assets/icons/icon_192x192.png',
+  './assets/icons/icon_256x256.png',
+  './assets/icons/icon_512x512.png',
 ];
 
 export const cacheFilesAfter = [
 
   // js
-  '../js/prod/client.js',
-  '../js/prod/app.js',
-  '../js/prod/sign.js',
-  '../js/prod/www.js',
+  './js/prod/client.js',
+  './js/prod/app.js',
+  './js/prod/sign.js',
+  './js/prod/www.js',
 
   // google
   // 'https://www.googletagmanager.com/gtag/js?id=UA-104340277-2',
@@ -26,27 +26,27 @@ export const cacheFilesAfter = [
   // 'https://www.google-analytics.com/analytics.js',
 
   // icons
-  '../icons/icon_57x57.png',
-  '../icons/icon_60x60.png',
-  '../icons/icon_72x72.png',
-  '../icons/icon_76x76.png',
-  '../icons/icon_114x114.png',
-  '../icons/icon_120x120.png',
-  '../icons/icon_144x144.png',
-  '../icons/icon_152x152.png',
-  '../icons/icon_180x180.png',
+  './assets/icons/icon_57x57.png',
+  './assets/icons/icon_60x60.png',
+  './assets/icons/icon_72x72.png',
+  './assets/icons/icon_76x76.png',
+  './assets/icons/icon_114x114.png',
+  './assets/icons/icon_120x120.png',
+  './assets/icons/icon_144x144.png',
+  './assets/icons/icon_152x152.png',
+  './assets/icons/icon_180x180.png',
 
   // splash
-  '../splash/screen_640x1136.png',
-  '../splash/screen_750x1334.png',
-  '../splash/screen_1125x2436.png',
-  '../splash/screen_1242x2208.png',
-  '../splash/screen_828x1792.png',
-  '../splash/screen_1242x2688.png',
-  '../splash/screen_1536x2048.png',
-  '../splash/screen_1668x2224.png',
-  '../splash/screen_1668x2388.png',
-  '../splash/screen_2048x2732.png',
+  './assets/splash/screen_640x1136.png',
+  './assets/splash/screen_750x1334.png',
+  './assets/splash/screen_1125x2436.png',
+  './assets/splash/screen_1242x2208.png',
+  './assets/splash/screen_828x1792.png',
+  './assets/splash/screen_1242x2688.png',
+  './assets/splash/screen_1536x2048.png',
+  './assets/splash/screen_1668x2224.png',
+  './assets/splash/screen_1668x2388.png',
+  './assets/splash/screen_2048x2732.png',
 
 ];
 
